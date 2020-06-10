c50c4bf5a0dc1dd6167007cdc066432ce818da9b
Kartik Narula <kartiknarula@cmu.edu>
2020-06-10 17:09:13 -0400

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,53 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+Process::Process(const int pid) : pid_(pid), cpuUsage_(CpuUtilization()) {}
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+int Process::Pid() { return pid_; }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+float Process::CpuUtilization() { 
+    const std::vector<long int> processInfo = LinuxParser::Cpu(pid_);
+    long int total_time = processInfo[processInfoElem::utime_] + processInfo[processInfoElem::stime_];
+    total_time += processInfo[processInfoElem::cutime_] + processInfo[processInfoElem::cstime_];
+    const float seconds = (float) Process::UpTime();
+    const float cpuUsage = ((total_time / sysconf(_SC_CLK_TCK)) / seconds);
+    return cpuUsage; 
+}
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+
+string Process::Command() { 
+    if (command_.length() == 0)
+        command_ = LinuxParser::Command(pid_);
+    return command_; 
+}
+
+
+string Process::Ram() { 
+    return LinuxParser::Ram(pid_);
+}
+
+
+string Process::User() { 
+    if (user_.length() == 0)
+        user_ = LinuxParser::User(pid_);
+    return user_; 
+}
+
+
+long int Process::UpTime() { 
+    return LinuxParser::UpTime(pid_); 
+}
+
+
+bool Process::operator>(Process const& a) const { 
+    return (cpuUsage_ > a.cpuUsage_);
+}
