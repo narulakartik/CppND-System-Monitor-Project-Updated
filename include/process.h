258f6a690fa7674197ec2c8dd54bec8603be0aeb
Kartik Narula <kartiknarula@cmu.edu>
2020-06-10 17:14:32 -0400

    Update process.h

--- include/process.h
+++ include/process.h
@@ -8,16 +8,27 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  Process(const int);  
+  int Pid();                               
+  std::string User();                      
+  std::string Command();                   
+  float CpuUtilization();                  
+  std::string Ram();                       
+  long int UpTime();                       
+  bool operator>(Process const& a) const;  
 
-  // TODO: Declare any necessary private members
  private:
+    int pid_;
+    std::string user_;
+    std::string command_ ;
+    float cpuUsage_ = 0;
+    enum processInfoElem {
+        utime_, 
+        stime_, 
+        cutime_, 
+        cstime_, 
+        starttime_
+    };
 };
 
 #endif
